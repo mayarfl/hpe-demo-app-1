b7a4f6b334f19e37d72329a67daf4ee3541842c6
lokshin <evgene.lokshin@microfocus.com>
2019-11-10 11:22:27 +0200

    quality story #899012. Rename Long Test

--- webapp/src/test/java/com/hp/devops/demoapp/UtilsTest.java
+++ webapp/src/test/java/com/hp/devops/demoapp/UtilsTest.java
@@ -27,7 +27,7 @@ public class UtilsTest {
 	}
 
 	@Test
-	public void test1WithVeeeeeeeeeeeeeeeeeeeeeryLooooooooooooooooooooooooooooooooooooooooooooooooooooooooongNaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaame() {
+	public void test1WithVeeeeeeeeeeeeeeeeeeryLooooooooooooooooooooooooooooooooooooooooooooooooooooooooongNaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaame() {
 		String[] result = Utils.nodify("");
 		assertEquals(result.length, 0);         //  0 is the right answer
 	}
