2e04b5b8a961efb988e4d94eca44629ce3c08a83
lokshin <evgene.lokshin@microfocus.com>
2019-11-13 09:56:58 +0200

    user story #2003: my user story

--- webapp/src/test/java/com/hp/devops/demoapp/UtilsTest.java
+++ webapp/src/test/java/com/hp/devops/demoapp/UtilsTest.java
@@ -27,7 +27,7 @@ public class UtilsTest {
 	}
 
 	@Test
-	public void test1WithVeeonaaaaaaaaaaaaaaaaaaaaaaaaaaaaaame() {
+	public void test1WithVeeonaaaaaaaaame() {
 		String[] result = Utils.nodify("");
 		assertEquals(result.length, 0);         //  0 is the right answer
 	}
